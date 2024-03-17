0a633de937ca5a2a2244dd1396993c8fc1dc32a4
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-17 05:47:36 +0100

    feat: change nav links to react componnents ✨

--- src/layouts/Nav/index.tsx
+++ src/layouts/Nav/index.tsx
@@ -1,19 +1,21 @@
+import { Link } from 'react-router-dom'
+
 export function Nav() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="/">
+      <Link className="main-nav-logo" to="/">
         <img
           className="main-nav-logo-image"
           src="img/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="/sign-in">
+        <Link className="main-nav-item" to="/sign-in">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   )
