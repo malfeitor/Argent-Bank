6a1a147d32ba81310f627ac154dd38e6899451c2
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-21 04:52:05 +0100

    feat: persist auth token ✨

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './utils/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './utils/store'
 import Landing from './pages/Landing'
 import { Nav } from './layouts/Nav'
 import { Footer } from './layouts/Footer'
@@ -12,15 +13,17 @@ import { UserPage } from './pages/User'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/user" element={<UserPage />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/user" element={<UserPage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,13 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/lib/persistStore'
 import authReducer from '../features/auth'
 import profileReducer from '../features/profile'
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+}
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
     profile: profileReducer,
   },
 })
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
