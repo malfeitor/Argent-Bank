f805c9cd6e7beb606a7c578857bf26290b666e1e
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-03-26 07:18:41 +0100

    feat: create Account component ✨

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from '../../utils/hooks'
 import { selectAuth } from '../../features/auth'
 import { Navigate } from 'react-router-dom'
 import { Greeter } from '../../layouts/Greeter'
+import Account from '../../components/Account'
 
 export function UserPage() {
   const auth = useAppSelector(selectAuth)
@@ -14,36 +15,21 @@ export function UserPage() {
     <main className="main bg-dark">
       <Greeter />
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <Account
+        title="Argent Bank Checking (x8349)"
+        amount="$2,082.79"
+        description="Available Balance"
+      />
+      <Account
+        title="Argent Bank Savings (x6712)"
+        amount="$10,928.42"
+        description="Available Balance"
+      />
+      <Account
+        title="Argent Bank Credit Card (x8349)"
+        amount="$184.30"
+        description="Current Balance"
+      />
     </main>
   )
 }
